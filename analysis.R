7b72cb10eb64c03a3df7994ae7c9eb4a3bc7c2a4
Sarah McKnight <sarah.mcknight0@gmail.com>
2018-02-08 16:47:52 -0700

    data manipulation -- I USED TIDYgit add analysis.R!

---
+++ analysis.R
@@ -0,0 +1,5 @@
+setwd("../../Comap2018/Data")
+energy <- read.csv("ProblemCData.csv", header=TRUE)
+colnames(energy) <- c("MSN", "state", "year", "data")
+library("tidyverse")
+energy_tidy <- spread(energy, key=MSN, value=data)

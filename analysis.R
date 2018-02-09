e9be9e9d005e69683858d603ca08ffbc0c52a6ba
Sarah McKnight <sarah.mcknight0@gmail.com>
2018-02-09 15:29:52 -0700

    I made a change

--- analysis.R
+++ analysis.R
@@ -1,5 +1,7 @@
-setwd("../../Comap2018/Data")
+setwd("../Comap2018/Data")
 energy <- read.csv("ProblemCData.csv", header=TRUE)
 colnames(energy) <- c("MSN", "state", "year", "data")
 library("tidyverse")
 energy_tidy <- spread(energy, key=MSN, value=data)
+energyList <- split(energy_tidy[,-1], f=energy_tidy$state)
+##this is a change
